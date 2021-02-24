0ed84aaecb82e5abc4abc287975394945ea990c4
SteveWLU <mccormicks@wlu.edu>
2021-02-24 12:57:22 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+const checklist = [
+  "Réflection 1",
+  "Réflection 2",
+  "Présentation 1",
+  "Présentation 2"
+];
+
 ReactDOM.render(<App />, document.getElementById("root"));
